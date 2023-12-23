5d19add4d51027a975d67ed41fc8cabd3a60fb2f
yeonhub-desktop <lsy3237@gmail.com>
2023-12-23 13:02:03 +0900

    [FEAT] AWS RDS MYSql 연결

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-    user: "root",
-    host: "localhost",
-    password: "12345",
-    database: "extblockdb",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 module.exports = connection;
